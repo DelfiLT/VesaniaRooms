da5858f7c9468cd4af342ea8cb460d1edef266d8
Delfina Tomasino <tomasinodelfina@gmail.com>
2024-04-21 22:34:01 -0300

    VSR-51: Color change mechanic

---
+++ Assets/ObjectColor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColor : MonoBehaviour
+{
+    [SerializeField] private Material primaryMaterial;
+    [SerializeField] private Material secondaryMaterial;
+
+    private MeshRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    void ChangeMaterial()
+    {
+        if (_renderer.material.name != secondaryMaterial.name + " (Instance)")
+        {
+            _renderer.material = secondaryMaterial;
+            return;
+        } 
+        if (_renderer.material.name != primaryMaterial.name + " (Instance)")
+        {
+            _renderer.material = primaryMaterial;
+        }
+    }
+
+    private void OnEnable()
+    {
+        ColorChange.OnChangeColor += ChangeMaterial;
+    }
+
+    private void OnDisable()
+    {
+        ColorChange.OnChangeColor -= ChangeMaterial;
+    }
+}
