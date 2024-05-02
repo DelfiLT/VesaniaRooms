bbb6fdfe0bcb2a04f73c8497d75672fae5f50563
Delfina Tomasino <tomasinodelfina@gmail.com>
2024-05-02 15:00:59 -0300

    VSR-81: Fix IAP

--- Assets/Scripts/Managers/LevelPurchase.cs
+++ Assets/Scripts/Managers/LevelPurchase.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
 using UnityEngine.UI;
 
 public class LevelPurchase : MonoBehaviour
 {
-    [SerializeField] private Button levelToUlock;
+    [SerializeField] private Button levelToUnlock;
+    [SerializeField] private Button buyButton;
 
-    public void PurchaseCompleted()
+    public void PurchaseCompleted(Product product)
     {
-        this.gameObject.SetActive(false);
-        levelToUlock.gameObject.SetActive(true);
+        buyButton.gameObject.SetActive(false);
+        levelToUnlock.gameObject.SetActive(true);
+        Debug.Log(product + "purchased");
+    }
+
+    public void PurchaseFailed(Product product, PurchaseFailureDescription failureDescription)
+    {
+        Debug.Log(product + "Failed for" + failureDescription);
+    }
+
+    public void ProductsFetch(Product product)
+    {
+        Debug.Log("Fetching products: " + product);
     }
 }
