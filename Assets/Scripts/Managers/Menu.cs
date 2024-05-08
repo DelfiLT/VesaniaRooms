3dcd5b807c65561482367fbff3127dbb40e08f03
Delfina Tomasino <tomasinodelfina@gmail.com>
2024-05-08 20:04:48 -0300

    Fix merge

--- Assets/Scripts/Managers/Menu.cs
+++ Assets/Scripts/Managers/Menu.cs
@@ -2,34 +2,4 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    private SwipeDetection _swipeDetection;
-
-    private void Awake()
-    {
-        _swipeDetection = SwipeDetection.Instance;
-    }
-
-    private void RotateRight()
-    {
-        transform.LeanRotateY(transform.eulerAngles.y + 90f, 1)
-            .setEaseInOutQuad();
-    }
-    
-    private void RotateLeft()
-    {
-        transform.LeanRotateY(transform.eulerAngles.y - 90f, 1)
-            .setEaseInOutQuad();
-    }
-
-    private void OnEnable()
-    {
-        _swipeDetection.OnSwipeRight += RotateRight;
-        _swipeDetection.OnSwipeLeft += RotateLeft;
-    }
-
-    private void OnDisable()
-    {
-        _swipeDetection.OnSwipeRight -= RotateRight;
-        _swipeDetection.OnSwipeLeft -= RotateLeft;
-    }
 }
--- Assets/Scripts/Mechanics/Rotation.cs
+++ Assets/Scripts/Mechanics/Rotation.cs
@@ -2,34 +2,15 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    private SwipeDetection _swipeDetection;
-
-    private void Awake()
-    {
-        _swipeDetection = SwipeDetection.Instance;
-    }
-
-    private void RotateRoomRight()
+    public void RotateRoomRight()
     {
         transform.LeanRotateY(transform.eulerAngles.y + 180f, 1)
             .setEaseInOutQuad();
     }
 
-    private void RotateRoomLeft()
+    public void RotateRoomLeft()
     {
         transform.LeanRotateY(transform.eulerAngles.y - 180f, 1)
             .setEaseInOutQuad();
-    }
-
-    private void OnEnable()
-    {
-        _swipeDetection.OnSwipeRight += RotateRoomRight;
-        _swipeDetection.OnSwipeLeft += RotateRoomLeft;
-    }
-
-    private void OnDisable()
-    {
-        _swipeDetection.OnSwipeRight -= RotateRoomRight;
-        _swipeDetection.OnSwipeLeft -= RotateRoomLeft;
     }
 }
