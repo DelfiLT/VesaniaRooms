f4d3b7677c2c391af2ec28cf91d4b57d398b24ac
Delfina Tomasino <tomasinodelfina@gmail.com>
2024-05-12 18:48:05 -0300

    VSR-85: Menu rotation and level manager

---
+++ Assets/Scripts/Managers/SceneData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScene", menuName = "SceneData")]
+public class SceneData : ScriptableObject
+{
+    [Header("Information")]
+    public string sceneName;
+    public string shortDescription;
+
+    [Header("Level Specific")]
+    public int puzzleQuantity;
+    public List<GameObject> clues;
+
+    //Se puede añadir mas especificaciones de las escenas a medida que vayamos necesitando, ej: sonidos.
+}
