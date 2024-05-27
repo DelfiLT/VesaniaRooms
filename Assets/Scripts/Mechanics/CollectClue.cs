86ad73c9653eb385267d09119d1af2de6be06e5b
Delfina Tomasino <tomasinodelfina@gmail.com>
2024-05-26 22:55:50 -0300

    VSR-80: collect clue

---
+++ Assets/Scripts/Mechanics/CollectClue.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectClue : MonoBehaviour
+{
+    [SerializeField] private GameObject notesClue;
+
+    [SerializeField] private LayerMask clueMask;
+    private InputManager inputManager;
+
+    private void Awake()
+    {
+        inputManager = InputManager.Instance;
+    }
+
+    private void OnTouch(Ray ray)
+    {
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, clueMask))
+        {
+            hit.transform.gameObject.SetActive(false);
+            notesClue.SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        inputManager.OnTouch += OnTouch;
+    }
+
+    private void OnDisable()
+    {
+        inputManager.OnTouch -= OnTouch;
+    }
+}
